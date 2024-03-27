9f8071ac015d4df5beec66c11bf5204576dc70fb
Piotrek00 <kotarbap93@gmail.com>
2024-03-27 16:39:33 +0100

    fix: app building error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import styles from "./page.module.css";
 import App from "./components/App";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <App />
+      <Suspense>
+        <App />
+      </Suspense>
     </main>
   );
 }
